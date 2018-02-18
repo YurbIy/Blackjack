46c1232e999bdb6a947db0de1af50cd2bdea1fe1
Iurii Novikov <yunov1@gmail.com>
2018-02-18 14:07:33 +0300

    Can play, croupier gets only two cards.

--- Blackjack.js
+++ Blackjack.js
@@ -1,3 +1,7 @@
+const readline = require('readline');
+
+const GREETING = 'Hello, there! Here is console Blackjack.'
+
 const ranges = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
 const suits = ['♥', '♦', '♣', '♠'];
@@ -10,11 +14,13 @@ const random = () => {
 }
 
 var hand = [];
+var croupiersHand = [];
 
-const takeCard = () => {
+const takeCard = (hand) => {
   var card = pile[random()];
   while(hand.indexOf(card) !== -1) card = pile[random()];
   hand.push(card);
+  return hand;
 }
 
 const isNumeric = (value) => {
@@ -28,6 +34,9 @@ const getCardWeigth = (card) => {
     case 'A':
       return 11;
       break;
+    case 'B':
+      return 1;
+      break;
     case 'J':
     case 'Q':
     case 'K':
@@ -36,15 +45,63 @@ const getCardWeigth = (card) => {
   }
 }
 
-const getHandWeight = () => {
+const getHandWeight = (hand) => {
   var result = 0;
-  hand.map((card) => {result += getCardWeigth(card)});
+  hand.map((card) => {
+    if(card[0] === 'A' && (result + 11) > 21){
+      card[0] = 'B';
+  }
+    result += getCardWeigth(card);
+  });
   return result;
 }
 
-takeCard();
-console.log(hand);
-console.log(getHandWeight());
-takeCard();
-console.log(hand);
-console.log(getHandWeight());
+const finalCount = () => {
+  const playersScore = getHandWeight(hand);
+  const croupiersScore = getHandWeight(croupiersHand);
+  console.log('Your final hand is:');
+  console.log(hand);
+  console.log('Croupier\'s final hand is:');
+  console.log(croupiersHand);
+  console.log('Your total score is:' + playersScore);
+  console.log('Croupier\'s total score is:' + croupiersScore);
+
+  if (playersScore > 21
+    || 21 - playersScore > 21 - croupiersScore) console.log('You lose!');
+  else console.log('You win!!!');
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+while (hand.length < 2) hand = takeCard(hand);
+while (croupiersHand.length < 2) croupiersHand = takeCard(croupiersHand);
+
+console.log('Here is your hand: ' + hand + '\nHere is croupier\'s first card:' + croupiersHand[0] + '\nOne more card?(y/n)');
+
+rl.on('line', (answer) => {
+  if(answer.toLowerCase().trim() === 'y') {
+    takeCard(hand);
+    console.log('Here is your hand: ' + hand);
+    if(getHandWeight(hand) < 21) {
+      rl.setPrompt('One more card?(y/n)')
+      rl.prompt();
+    } else {
+      finalCount();
+      rl.close();
+    }
+  }
+  else if (answer.toLowerCase().trim() === 'n') {
+    finalCount();
+    rl.close();
+    return;
+  } else {
+    rl.setPrompt('Please type exacly y or n!')
+    rl.prompt();
+  }
+});
+
+
+
